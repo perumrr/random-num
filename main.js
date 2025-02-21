0213f894dd8442c84c0d94a3ed9843da3b61c481
perumrr <74992461+perumrr@users.noreply.github.com>
2025-02-21 19:51:14 +0000

    adding iniital code

---
+++ main.js
@@ -0,0 +1,13 @@
+<script src="/assets/js/main.js" defer></script>
+
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('randomNameGenerator', () => ({
+        names: ["Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Hank", "Ivy", "Jack"],
+        selectedName: "Click the button to see a name",
+        pickRandomName() {
+            let randomIndex = Math.floor(Math.random() * this.names.length);
+            this.selectedName = this.names[randomIndex];
+        }
+    }));
+});
